120066f17a31b6573963660452076a09506518ab
kangtaehyeon <s25002@gsm.hs.kr>
2025-06-23 19:36:01 +0900

    1

--- quiz.c
+++ quiz.c
@@ -1,80 +1,110 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <emscripten/emscripten.h>
 #include <string.h>
-#include <emscripten.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 #define MAX_QUESTIONS 100
-#define MAX_LINE 256
 #define SELECT_COUNT 10
+#define MAX_OPTION_LEN 128
 
 typedef struct {
     char question[256];
-    char options[5][128];
-    int answer;
+    char options[5][MAX_OPTION_LEN];
+    int answer; // 1~5
 } Question;
 
 Question allQuestions[MAX_QUESTIONS];
-Question selectedQuestions[SELECT_COUNT];
 int totalQuestions = 0;
+Question selectedQuestions[SELECT_COUNT];
 
+// 외부에서 문제를 불러올 함수 (예: JS에서 직접 세팅 가능)
 EMSCRIPTEN_KEEPALIVE
-void loadQuestions() {
-    FILE *file = fopen("question.txt", "r");
-    if (!file) return;
-
-    while (fgets(allQuestions[totalQuestions].question, sizeof(allQuestions[totalQuestions].question), file)) {
-        allQuestions[totalQuestions].question[strcspn(allQuestions[totalQuestions].question, "\n")] = '\0';
-
-        for (int i = 0; i < 5; i++) {
-            fgets(allQuestions[totalQuestions].options[i], sizeof(allQuestions[totalQuestions].options[i]), file);
-            allQuestions[totalQuestions].options[i][strcspn(allQuestions[totalQuestions].options[i], "\n")] = '\0';
-        }
-
-        char answerLine[10];
-        fgets(answerLine, sizeof(answerLine), file);
-        allQuestions[totalQuestions].answer = atoi(answerLine);
-        totalQuestions++;
-    }
-
-    fclose(file);
+void loadQuestions(Question* questions, int count) {
+    memcpy(allQuestions, questions, sizeof(Question) * count);
+    totalQuestions = count;
+    srand((unsigned int)time(NULL));
 }
 
+// 보기 섞기 함수
+void shuffleOptions(Question* q) {
+    typedef struct {
+        char text[MAX_OPTION_LEN];
+        int originalIndex;
+    } Option;
+
+    Option tempOptions[5];
+    int correctIndex = q->answer - 1;
+
+    for (int i = 0; i < 5; i++) {
+        strcpy(tempOptions[i].text, q->options[i]);
+        tempOptions[i].originalIndex = i;
+    }
+
+    for (int i = 4; i > 0; i--) {
+        int j = rand() % (i + 1);
+        Option tmp = tempOptions[i];
+        tempOptions[i] = tempOptions[j];
+        tempOptions[j] = tmp;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        strcpy(q->options[i], tempOptions[i].text);
+        if (tempOptions[i].originalIndex == correctIndex) {
+            q->answer = i + 1;
+        }
+    }
+}
+
+// 랜덤 문제 10개 선택 및 정답 보기 섞기
 EMSCRIPTEN_KEEPALIVE
 void pickRandomQuestions() {
     for (int i = 0; i < SELECT_COUNT; i++) {
         int idx = rand() % totalQuestions;
         selectedQuestions[i] = allQuestions[idx];
+        shuffleOptions(&selectedQuestions[i]);
     }
 }
 
+// 문제 JSON 형식으로 얻기 (quiz.js에서 문자열로 받을 수 있게)
 EMSCRIPTEN_KEEPALIVE
-const char* getQuestionJSON(int index) {
-    static char buffer[1024];
-    if (index < 0 || index >= SELECT_COUNT) return "{}";
+const char* getQuestionJSON(int idx) {
+    static char buffer[2048];
+    if (idx < 0 || idx >= SELECT_COUNT) return "";
+
+    Question* q = &selectedQuestions[idx];
+
+    char optionsJson[1024] = "";
+    for (int i = 0; i < 5; i++) {
+        char optEscaped[256] = {0};
+        // 간단하게 큰따옴표만 이스케이프
+        int j=0, k=0;
+        while(q->options[i][j] && k<255) {
+            if (q->options[i][j] == '"') {
+                optEscaped[k++] = '\\';
+                optEscaped[k++] = '"';
+            } else {
+                optEscaped[k++] = q->options[i][j];
+            }
+            j++;
+        }
+        optEscaped[k] = 0;
+        char temp[256];
+        if(i < 4) snprintf(temp, sizeof(temp), "\"%s\",", optEscaped);
+        else snprintf(temp, sizeof(temp), "\"%s\"", optEscaped);
+        strcat(optionsJson, temp);
+    }
 
     snprintf(buffer, sizeof(buffer),
-        "{\"question\":\"%s\",\"options\":[\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"]}",
-        selectedQuestions[index].question,
-        selectedQuestions[index].options[0],
-        selectedQuestions[index].options[1],
-        selectedQuestions[index].options[2],
-        selectedQuestions[index].options[3],
-        selectedQuestions[index].options[4]
-    );
+        "{\"question\":\"%s\", \"options\":[%s], \"answer\":%d}",
+        q->question,
+        optionsJson,
+        q->answer);
+
     return buffer;
 }
 
 EMSCRIPTEN_KEEPALIVE
-int gradeAnswers(int* userAnswers) {
-    int correct = 0;
-    for (int i = 0; i < SELECT_COUNT; i++) {
-        if (userAnswers[i] == selectedQuestions[i].answer) correct++;
-    }
-    return correct;
+int getSelectedCount() {
+    return SELECT_COUNT;
 }
 
-// 새로 추가: getRandomNumber 함수 (컴파일 시 export할 함수)
-EMSCRIPTEN_KEEPALIVE
-int getRandomNumber() {
-    return rand();
-}
